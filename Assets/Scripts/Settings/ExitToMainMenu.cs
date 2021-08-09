2a2aeaaef1ba5b55d9fbdae0f37737836bb538f3
buldosik <60583195+buldosik@users.noreply.github.com>
2021-08-09 17:07:54 +0300

    SettingExit
    ДОБАВИЛ КНОПКУ ВЫХОД ИЗ НАСТРОЕК

---
+++ Assets/Scripts/Settings/ExitToMainMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class ExitToMainMenu : MonoBehaviour
+{
+    [SerializeField]
+    private Button _MainMenu;
+
+
+    void Start()
+    {
+        _MainMenu.onClick.AddListener(_MainMenuClick);
+    }
+
+    void _MainMenuClick()
+    {
+        SceneManager.LoadScene("Main_menu");
+    }
+}
